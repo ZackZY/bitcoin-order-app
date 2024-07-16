bb0e8a9218e606e392ee13fae485a3ea704fe30c
Zi Yang Fu <ziyang.fu@ncs.com.sg>
2024-07-16 16:10:58 +0800

    Correct Git Pages URL

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker pull zaproxy/zap-stable
-docker run -i zaproxy/zap-stable zap-baseline.py -t "https://github.com/ZackZY/bitcoin-order-app.git" -l PASS > zap_baseline_report.html
+docker run -i zaproxy/zap-stable zap-baseline.py -t "https://zackzy.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
 
 echo $? > /dev/null
